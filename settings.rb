5627369d0c37aafd1e75d1d635f44f769ce2b900
channelflipper <channelflipper@channelflipper-VirtualBox.(none)>
2014-04-16 02:45:37 -0700

    removing redis references

--- settings.rb
+++ settings.rb
@@ -11,30 +11,10 @@ configure :development do
     DataMapper.setup(:default, ENV['DATABASE_URL'] )
 end
 configure :production do
-    require 'newrelic_rpm'
     DataMapper.setup(:default, ENV['DATABASE_URL'] )
 end
 
 def setting_initialize()
-
-    configure :production do
-        uri = URI.parse(ENV["REDISCLOUD_URL"])
-        redis_pusher = Redis.new(host: uri.host, port: uri.port, password: uri.password)
-        set :redischatchannel, "chat-demo"
-        set :redispusher, redis_pusher
-    end
-    configure :development do
-        uri = URI.parse(ENV["REDISCLOUD_URL"])
-        redis_pusher = Redis.new(host: uri.host, port: uri.port, password: uri.password)
-        set :redischatchannel, "chat-demo"
-        set :redispusher, redis_pusher
-    end
-    configure :test do
-        redis_pusher = Redis.new
-        set :redischatchannel, "chat-demo"
-        set :redispusher, redis_pusher
-    end
-
 
     puts "SETTINGS INITIALIZE"
     
