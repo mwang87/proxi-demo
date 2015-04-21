dc8bf95ba8ef019612bdb7889dcdf53c7e9f88fb
mingxun <mingxun@microming.(none)>
2015-04-20 17:41:36 -0700

    updating the dataset page

--- controllers/dataset_controller.rb
+++ controllers/dataset_controller.rb
@@ -81,21 +81,24 @@ end
 
 #Aggregate View
 get '/dataset/aggregateview' do
+    page_number, @previous_page, @next_page = page_prev_next_utilties(params)
+
     protein = params[:protein]
     peptide = params[:peptide]
     modification = params[:mod]
 
-    if protein = nil
+    if protein == nil
         protein = ""
     end
 
-    if peptide = nil
+    if peptide == nil
         peptide = ""
     end
 
-    if modification = nil
+    if modification == nil
         modification = ""
     end
+
 
     filter_protein = false
     filter_peptide = false
@@ -105,85 +108,103 @@ get '/dataset/aggregateview' do
     peptide_db = nil
     protein_db = nil
     mod_db = nil
-    dataset_variants_mod_db = nil
-    dataset_peptide_db = nil
-    dataset_protein_db = nil
 
     if protein.length > 2
         filter_protein = true
         protein_db = Protein.first(:name => protein)
-        dataset_protein_db = protein_db.DatasetProtein
     end
 
     if peptide.length > 2
         filter_peptide = true
         query_peptide = "%" + peptide + "%"
         peptide_db = Peptide.first(:sequence.like => query_peptide)
-        dataset_peptide_db = peptide_db.DatasetPeptide
     end
 
     if modification.length > 2
         filter_mod = true
         mod_db = Modification.first(:name => modification)
-        dataset_variants_mod_db = DatasetVariant.all(:variant => mod_db.variants)
     end
 
     #Now we do a big switch statement
     if filter_protein and filter_peptide and filter_mod
         @datasets = Dataset.all(
-            :DatasetPeptide => dataset_peptide_db, 
-            :DatasetVariant => dataset_variants_mod_db, 
-            :DatasetProtein => dataset_protein_db)
+            :datasetpeptide => DatasetPeptide.all(:peptide => peptide_db),
+            :datasetprotein => DatasetProtein.all(:protein => protein_db),
+            :datasetmodification => DatasetModification.all(:modification => mod_db),
+            :offset => (page_number - 1) * PAGINATION_SIZE , 
+            :limit => PAGINATION_SIZE
+            )
 
-        return haml :dataset_plain_display
+        return haml :dataset_aggregate
     end
 
     if filter_peptide and filter_mod
         @datasets = Dataset.all(
-            :DatasetPeptide => dataset_peptide_db, 
-            :DatasetVariant => dataset_variants_mod_db)
+            :datasetpeptide => DatasetPeptide.all(:peptide => peptide_db),
+            :datasetmodification => DatasetModification.all(:modification => mod_db),
+            :offset => (page_number - 1) * PAGINATION_SIZE , 
+            :limit => PAGINATION_SIZE
+            )
 
-        return haml :dataset_plain_display
+        return haml :dataset_aggregate
     end
 
     if filter_protein and filter_mod
         @datasets = Dataset.all(
-            :DatasetVariant => dataset_variants_mod_db, 
-            :DatasetProtein => dataset_protein_db)
+            :datasetprotein => DatasetProtein.all(:protein => protein_db),
+            :datasetmodification => DatasetModification.all(:modification => mod_db),
+            :offset => (page_number - 1) * PAGINATION_SIZE , 
+            :limit => PAGINATION_SIZE
+            )
 
-        return haml :dataset_plain_display
+        return haml :dataset_aggregate
     end
 
     if filter_protein and filter_peptide
         @datasets = Dataset.all(
-            :DatasetPeptide => dataset_peptide_db,
-            :DatasetProtein => dataset_protein_db)
+            :datasetpeptide => DatasetPeptide.all(:peptide => peptide_db),
+            :datasetprotein => DatasetProtein.all(:protein => protein_db),
+            :offset => (page_number - 1) * PAGINATION_SIZE , 
+            :limit => PAGINATION_SIZE
+            )
 
-        return haml :dataset_plain_display
+        return haml :dataset_aggregate
     end
 
     if filter_protein
         @datasets = Dataset.all(
-            :DatasetProtein => dataset_protein_db)
+            :datasetprotein => DatasetProtein.all(:protein => protein_db),
+            :offset => (page_number - 1) * PAGINATION_SIZE , 
+            :limit => PAGINATION_SIZE
+            )
 
-        return haml :dataset_plain_display
+        return haml :dataset_aggregate
     end
 
     if filter_peptide
         @datasets = Dataset.all(
-            :DatasetPeptide => dataset_peptide_db)
+            :datasetpeptide => DatasetPeptide.all(:peptide => peptide_db),
+            :offset => (page_number - 1) * PAGINATION_SIZE , 
+            :limit => PAGINATION_SIZE
+            )
 
-        return haml :dataset_plain_display
+        return haml :dataset_aggregate
     end
 
     if filter_mod
         @datasets = Dataset.all(
-            :DatasetVariant => dataset_variants_mod_db)
+            :datasetmodification => DatasetModification.all(:modification => mod_db),
+            :offset => (page_number - 1) * PAGINATION_SIZE , 
+            :limit => PAGINATION_SIZE
+            )
 
-        return haml :dataset_plain_display
+        return haml :dataset_aggregate
     end
 
-    return "MING"
+    @datasets = Dataset.all(:offset => (page_number - 1) * PAGINATION_SIZE , 
+            :limit => PAGINATION_SIZE)
+
+    return haml :dataset_aggregate
 end
 
 
