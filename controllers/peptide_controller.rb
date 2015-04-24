588dc9fbe959a0424da2874d511111238bc32bf4
mingxun <mingxun@ubuntu.(none)>
2015-04-23 23:00:29 -0700

    allowing users to sort

--- controllers/peptide_controller.rb
+++ controllers/peptide_controller.rb
@@ -62,10 +62,14 @@ end
 #Aggregate View
 get '/peptide/aggregateview' do
     page_number, @previous_page, @next_page = page_prev_next_utilties(params)
+    @page_number = page_number
 
     protein = params[:protein]
     peptide = params[:peptide]
     modification = params[:mod]
+
+    sort_direction = params[:sort]
+    sort_type = params[:sorttype]
 
     if protein == nil
         protein = ""
@@ -100,131 +104,47 @@ get '/peptide/aggregateview' do
     protein_db = nil
     mod_db = nil
 
-    variants_protein_db = nil
+    query_parameters = Hash.new
+    query_parameters[:offset]  = (page_number - 1) * PAGINATION_SIZE
+    query_parameters[:limit]  = PAGINATION_SIZE
+
+    count_parameters = Hash.new
 
     if protein.length > 2
         filter_protein = true
         protein_db = Protein.first(:name => protein)
+        query_parameters[:peptideprotein] = PeptideProtein.all(:protein => protein_db)
+        count_parameters[:peptideprotein] = PeptideProtein.all(:protein => protein_db)
     end
 
     if peptide.length > 2
         filter_peptide = true
         query_peptide = "%" + peptide + "%"
         peptides_db = Peptide.all(:sequence.like => query_peptide)
+        query_parameters[:sequence.like] = query_peptide
+        count_parameters[:sequence.like] = query_peptide
     end
 
     if modification.length > 2
         filter_mod = true
         mod_db = Modification.first(:name => modification)
+        query_parameters[:modificationpeptide] = ModificationPeptide.all(:modification => mod_db)
+        count_parameters[:modificationpeptide] = ModificationPeptide.all(:modification => mod_db)
     end
 
-    #Now we do a big switch statement
-    if filter_protein and filter_peptide and filter_mod
-        @all_peptides, @total_count = peptide_query_for_peptide_protein_mod(protein, 
-            modification, peptide, page_number)
-
-        return haml :peptide_aggregate
+    #Determining the sorting direction and for what field
+    if sort_type == "sequence"
+        if sort_direction == "up"
+            query_parameters[:order] = [:sequence.desc]
+        else
+            query_parameters[:order] = [:sequence.asc]
+        end
     end
 
-    if filter_protein and filter_peptide
-        @all_peptides = peptides_db.all(
-            :peptideprotein => PeptideProtein.all(:protein => protein_db),
-            :offset => (page_number - 1) * PAGINATION_SIZE, 
-            :limit => PAGINATION_SIZE)
-
-        @total_count = peptides_db.count(
-            :peptideprotein => PeptideProtein.all(:protein => protein_db));
-
-        return haml :peptide_aggregate
-    end
-
-    if filter_peptide and filter_mod
-        @all_peptides = peptides_db.all(
-            :modificationpeptide => {:modification => mod_db},
-            :offset => (page_number - 1) * PAGINATION_SIZE, 
-            :limit => PAGINATION_SIZE)
-
-        @total_count = peptides_db.count(
-            :modificationpeptide => {:modification => mod_db});
-
-        return haml :peptide_aggregate
-    end
-
-    if filter_protein and filter_mod
-        @all_peptides = Peptide.all(
-            :peptideprotein => PeptideProtein.all(:protein => protein_db),
-            :modificationpeptide => {:modification => mod_db},
-            :offset => (page_number - 1) * PAGINATION_SIZE, 
-            :limit => PAGINATION_SIZE)
-
-        @total_count = Peptide.count(
-            :peptideprotein => PeptideProtein.count(:protein => protein_db),
-            :modificationpeptide => {:modification => mod_db})
-
-        return haml :peptide_aggregate
-    end
-
-    if filter_protein
-        @all_peptides = Peptide.all(
-            :peptideprotein => PeptideProtein.all(:protein => protein_db),
-            :offset => (page_number - 1) * PAGINATION_SIZE, 
-            :limit => PAGINATION_SIZE)
-
-        @total_count = Peptide.count(
-            :peptideprotein => PeptideProtein.all(:protein => protein_db));
-
-        return haml :peptide_aggregate
-    end
-
-    if filter_peptide
-        @all_peptides = peptides_db.all(
-            :offset => (page_number - 1) * PAGINATION_SIZE , 
-            :limit => PAGINATION_SIZE)
-
-        @total_count = peptides_db.count
-
-        return haml :peptide_aggregate
-    end
-
-    if filter_mod
-        @all_peptides = Peptide.all(
-            :modificationpeptide => {:modification => mod_db},
-            :offset => (page_number - 1) * PAGINATION_SIZE , 
-            :limit => PAGINATION_SIZE)
-
-        @total_count = Peptide.count(
-            :modificationpeptide => {:modification => mod_db});
-
-        return haml :peptide_aggregate
-    end
-
-    ###Need to write custom SQL to properly do the joins with modifications
-
-    @all_peptides = Peptide.all(
-            :offset => (page_number - 1) * PAGINATION_SIZE , 
-            :limit => PAGINATION_SIZE)
-
-    @total_count = Peptide.count
+    @all_peptides = Peptide.all(query_parameters)
+    @total_count = Peptide.count(count_parameters)
 
     return haml :peptide_aggregate
-end
 
-#Returns the query result and the counts of the total result set
-def peptide_query_for_peptide_protein_mod(protein, mod, peptide, page_number)
-    protein_db = Protein.first(:name => protein)
-    mod_db = Modification.first(:name => mod)
 
-    all_peptides = Peptide.all(
-            :peptideprotein => PeptideProtein.all(:protein => protein_db),
-            :modificationpeptide => {:modification => mod_db},
-            :sequence => peptide,
-            :offset => (page_number - 1) * PAGINATION_SIZE, 
-            :limit => PAGINATION_SIZE)
-
-    total_count = Peptide.count(
-        :peptideprotein => PeptideProtein.count(:protein => protein_db),
-        :modificationpeptide => {:modification => mod_db},
-        :sequence => peptide);
-
-    return all_peptides, total_count
 end
