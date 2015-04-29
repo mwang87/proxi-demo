bcb0895c4b584a82bea0cef3b23ddf0883054757
mingxun <mingxun@ubuntu.(none)>
2015-04-28 19:55:11 -0700

    updating settings so that we can use it on heroku

--- settings.rb
+++ settings.rb
@@ -2,24 +2,24 @@ require 'data_mapper'
 require 'redis'
 
 
-DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/data.db")
-#DataMapper.setup(:default, 'postgres://postgres:postgres@localhost/proxi-demo' )
+
 
 PAGINATION_SIZE = 10
 
-# puts ENV["RACK_ENV"]
-# configure :test do
-#     
-# end
-# configure :development do
-#     DataMapper.setup(:default, ENV['DATABASE_URL'] )
-# end
-# configure :production do
-#     DataMapper.setup(:default, ENV['DATABASE_URL'] )
-# end
-# 
-# def setting_initialize()
-# 
-#     puts "SETTINGS INITIALIZE"
-#     
-# end
+puts ENV["RACK_ENV"]
+configure :test do
+    #DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/data.db")
+	DataMapper.setup(:default, 'postgres://postgres:postgres@localhost/proxi-demo' )
+end
+configure :development do
+    DataMapper.setup(:default, ENV['DATABASE_URL'] )
+end
+configure :production do
+    DataMapper.setup(:default, ENV['DATABASE_URL'] )
+end
+
+def setting_initialize()
+
+    puts "SETTINGS INITIALIZE"
+    
+end
