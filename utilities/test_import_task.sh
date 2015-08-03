2916451dbb18b6bb0af85adf1846da2558473d13
mingxun <mingxun@ubuntu.(none)>
2015-08-02 18:00:02 -0700

    updating view of datasets

--- utilities/test_import_task.sh
+++ utilities/test_import_task.sh
@@ -1,2 +1,3 @@
 #ruby ./populate_db_task.rb 0654282b35b14cbd8a0c8bb901486118
-ruby ./populate_db_task.rb 89c06400840b408ba642133a812cace4
+#ruby ./populate_db_task.rb 89c06400840b408ba642133a812cace4
+ruby ./populate_db_task.rb 944c7f1488ae40379bbfa351eeb9b896
