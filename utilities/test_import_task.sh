9725b9bb08e51f338fadf5e31192d8431c7d4f3b
mingxun <mingxun@microming.(none)>
2015-08-21 17:45:20 -0700

    adding small task

--- utilities/test_import_task.sh
+++ utilities/test_import_task.sh
@@ -4,10 +4,14 @@
 #ruby ./populate_db_task.rb 1c80df047f4d4dc58982aa5e1b892fb3
 
 #ruby ./populate_db_task.rb 3a41b8cc22724e138f25df1e75636b80 && sleep 5
-ruby ./populate_db_task.rb 8b2ac12926af41d69df73aa00c8978f2 && sleep 10 
-ruby ./populate_db_task.rb 672cdd4d3494468eb49c84e41f0016c2 && sleep 10
+#ruby ./populate_db_task.rb 8b2ac12926af41d69df73aa00c8978f2 && sleep 10 
+#ruby ./populate_db_task.rb 672cdd4d3494468eb49c84e41f0016c2 && sleep 10
 #ruby ./populate_db_task.rb  && sleep 5
 #ruby ./populate_db_task.rb  && sleep 5
 #ruby ./populate_db_task.rb  && sleep 5
 #ruby ./populate_db_task.rb  && sleep 5
 #ruby ./populate_db_task.rb  && sleep 5
+
+
+#Small SILAC
+ruby ./populate_db_task.rb 5db08b8b5691403ca860b0dc3aa49eb9
